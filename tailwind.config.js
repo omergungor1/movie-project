b0fda508c813c8dea737af85b6ba1ac6e7718192
Ömer <omergungorco@gmail.com>
2024-06-16 16:46:29 +0300

    Header component done.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        lg: "1140px",
+        xl: "1140px",
+        '2xl': "1140px",
+      },
+    },
+    extend: {
+      colors: {
+        gega: {
+          'red': '#BC1A45',
+          'melon': '#FFD369',
+          'grey': '#DDDDDD',
+          'white': '#f7f7f7',
+        }
+      },
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
