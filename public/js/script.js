955a599489ae5270f22649bd346eed0c0e28bf6b
Ömer <omergungorco@gmail.com>
2024-06-18 15:22:40 +0300

    bug fixes

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,28 @@
 const sunIcon = document.querySelector('#sun');
 const moonIcon = document.querySelector('#moon');
+const sunIcon2 = document.querySelector('#sun2');
+const moonIcon2 = document.querySelector('#moon2');
 const body = document.querySelector('body');
 
 sunIcon.addEventListener('click', () => {
     body.classList.remove('dark');
     sunIcon.classList.add('hidden');
     moonIcon.classList.remove('hidden');
-
-    // Save the theme preference for the user
-    localStorage.setItem('theme', 'light');
 });
 
 moonIcon.addEventListener('click', () => {
     body.classList.add('dark');
     moonIcon.classList.add('hidden');
     sunIcon.classList.remove('hidden');
-
-    // Save the theme preference for the user
-    localStorage.setItem('theme', 'dark');
+});
+sunIcon2.addEventListener('click', () => {
+    body.classList.remove('dark');
+    sunIcon2.classList.add('hidden');
+    moonIcon2.classList.remove('hidden');
 });
 
-// Check the user's theme preference
-// const theme = localStorage.getItem('theme');
-// console.log(theme);
-// if (theme === 'dark') {
-//     body.classList.add('dark');
-//     moonIcon.classList.add('hidden');
-//     sunIcon.classList.remove('hidden');
-// } else {
-//     body.classList.remove('dark');
-//     moonIcon.classList.add('hidden');
-//     sunIcon.classList.remove('hidden');
-// }
+moonIcon2.addEventListener('click', () => {
+    body.classList.add('dark');
+    moonIcon2.classList.add('hidden');
+    sunIcon2.classList.remove('hidden');
+});
